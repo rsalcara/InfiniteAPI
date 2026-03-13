cd9b495c61eb1c21af3aa58b3856f1d58324b2c1
Matheus Filype <67132916+Santosl2@users.noreply.github.com>
2026-03-13 14:49:16 -0300

    feat: groups mention all  (#2396)
    * fix: improve message resend logic by adding checks for message IDs
    
    * Revert "fix: improve message resend logic by adding checks for message IDs"
    
    This reverts commit c03f9d8e6fc6cbfbb9d1f8f67c169700e704213d.
    
    * feat: add mentionAll support to message context for group mentions
    
    * fix: improve readability of condition for mentions and mentionAll in generateWAMessageContent
    
    * Apply suggestions from code review
    
    Co-authored-by: Rajeh Taher <rajeh@reforward.dev>
    
    ---------
    
    Co-authored-by: Rajeh Taher <rajeh@reforward.dev>

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -1760,15 +1760,21 @@ export const generateWAMessageContent = async (
 		m = { viewOnceMessage: { message: m } }
 	}
 
-	if (hasOptionalProperty(message, 'mentions') && message.mentions?.length) {
+	if (
+		(hasOptionalProperty(message, 'mentions') && message.mentions?.length) ||
+		(hasOptionalProperty(message, 'mentionAll') && message.mentionAll)
+	) {
 		const messageType = Object.keys(m)[0] as Extract<keyof proto.IMessage, MessageWithContextInfo>
 		if (messageType) {
 			const key = m[messageType]
-			if (key && 'contextInfo' in key && !!key.contextInfo) {
-				key.contextInfo.mentionedJid = message.mentions
-			} else if (key) {
-				key.contextInfo = {
-					mentionedJid: message.mentions
+			if (key && 'contextInfo' in key) {
+				key.contextInfo = key.contextInfo || {}
+				if (message.mentions?.length) {
+					key.contextInfo.mentionedJid = message.mentions
+				}
+
+				if (message.mentionAll) {
+					key.contextInfo.nonJidMentions = 1
 				}
 			}
 		}
